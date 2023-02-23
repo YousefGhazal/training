2a30a217e92ee44d10db18dcbaa178fa1721b3b5
Yousef <yousefghazal2412@gmail.com>
2023-02-23 14:37:51 +0200

    last func

--- social-media/models.py
+++ social-media/models.py
@@ -54,67 +54,65 @@ user = User("yousef", "yasser", "email.com")
 user1 = User("yousef", "yasser", "email.com")
 pro = Profile("bbb", ["ll", "pp"], user1)
 post = Post("post text hh", user1)
-com = Comment("rrr", post, user1)
+com = Comment("post", post, user1)
+
+
+# - get data  for each class
+key = input()
+def get_data(key):
+    arr = {
+        "User": User.data,
+        "Profile": Profile.data,
+        "Post": Post.data,
+        "Like": Like.data,
+        "Comment": Comment.data,
+    }
+    if key in arr.keys() :
+        print(arr[key])
+get_data(key)
+
+
+# - input => post.text , output => list of data form comments
+k_post = input()
+def get_comments(post):
+    comments = []
+    for i in Comment.data:
+        if i.post.text == post:
+            comments.append(i)
+    print(comments)
+get_comments(k_post)
+
 
 # - input => keyword , output => list of posts 
-print(Post.data[0].text)
 p_word = input()
 def get_posts_by_keyword(word):
     for i in Post.data:
         arr = i.text.split()
         if word in arr:
             print(i)
-
 get_posts_by_keyword(p_word)
 
-# input => post.text , output => list of data form comment
-# k_post = input()
-# def get_comments(post):
-#     comments = []
-#     for i in Comment.data:
-#         if i.post.text == post:
-#             comments.append(i)
-#     print(comments)
-
-
-# get_comments(k_post)
 
 # input => user.fname , output => list of data form post
-
-# u_post = input()
-
-
-# def get_posts(user):
-#     posts = []
-#     for i in Post.data:
-#         if i.user.fname == user:
-#             posts.append(i)
-#     print(posts)
+u_post = input()
+def get_posts(user):
+    posts = []
+    for i in Post.data:
+        if i.user.fname == user:
+            posts.append(i)
+    print(posts)
+get_posts(u_post)
 
 
-# get_posts(u_post)
-
-
-"""
-- save class data in list 
-- input => class name , output => class data
-"""
-# key = input()
-# def get_data(key):
-#     arr = {
-#         "User": User.data,
-#         "Profile": Profile.data,
-#         "Post": Post.data,
-#         "Like": Like.data,
-#         "Comment": Comment.data,
-#     }
-#     if key in arr.keys() :
-#         print(arr[key])
-
-
-# get_data(key)
-
-
-"""
-
-"""
+# - input => keyword , output => posts and comments 
+word = input()
+def get_by_word(word):
+    for i in Post.data:
+        arr = i.text.split()
+        if word in arr:
+            print(i)
+    for i in Comment.data:
+        arr = i.text.split()
+        if word in arr:
+            print(i)
+get_by_word(word)
